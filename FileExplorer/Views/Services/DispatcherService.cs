836db5b1f1994cacb59aae0adf08fa8bba0c54fe
Alex Chuang <1392alex1392@gmail.com>
2020-06-16 07:34:17 +0100

    add dispatcherservice

---
+++ FileExplorer/Views/Services/DispatcherService.cs
@@ -0,0 +1,40 @@
+﻿using FileExplorer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace FileExplorer.Views.Services
+{
+	public class DispatcherService : IDispatcherService
+	{
+		#region Public Properties
+
+		public Dispatcher Dispatcher
+		{
+			get
+			{
+				if (Application.Current == null ||
+					Application.Current.Dispatcher == null)
+				{
+					throw new InvalidOperationException();
+				}
+				return Application.Current.Dispatcher;
+			}
+		}
+
+		#endregion Public Properties
+
+		#region Public Methods
+
+		public void Invoke(Action action)
+		{
+			Dispatcher.Invoke(action);
+		}
+
+		#endregion Public Methods
+	}
+}
