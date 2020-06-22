79c4a7d9c94b42611f84f2258c1796ca0f478695
Alex Chuang <1392alex1392@gmail.com>
2020-06-22 10:52:45 +0100

    add okcommand binding

--- FileExplorer/Views/FileNameInputWindow.xaml.cs
+++ FileExplorer/Views/FileNameInputWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FileExplorer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,25 +15,65 @@ using System.Windows.Shapes;
 
 namespace FileExplorer.Views
 {
+	
 	/// <summary>
 	/// Interaction logic for FileNameInputWindow.xaml
 	/// </summary>
 	public partial class FileNameInputWindow : Window
 	{
+		public class OkButtonCommand : ICommand
+		{
+			private readonly Action action;
+			private readonly Func<bool> canAction;
+
+			public event EventHandler CanExecuteChanged;
+			public OkButtonCommand(Action action, Func<bool> canAction = null)
+			{
+				this.action = action;
+				this.canAction = canAction;
+			}
+			public void OnCanExecuteChanged()
+			{
+				CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+			}
+			public bool CanExecute(object parameter)
+			{
+				return canAction == null || canAction.Invoke();
+			}
+
+			public void Execute(object parameter)
+			{
+				action.Invoke();
+			}
+		}
 		public FileNameInputWindow()
 		{
 			InitializeComponent();
+			this.DataContext = this;
+			OkCommand = new OkButtonCommand(OK, CanOK);
 		}
 
-		private void OKButton_Click(object sender, RoutedEventArgs e)
+		public OkButtonCommand OkCommand { get; private set; }
+
+		private void OK()
 		{
 			DialogResult = true;
 			Close();
 		}
 
+		private bool CanOK()
+		{
+			return !string.IsNullOrEmpty(fileNameTextBox.Text);
+		}
+
 		private void CancelButton_Click(object sender, RoutedEventArgs e)
 		{
 			Close();
+		}
+
+		private void fileNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			OkCommand.OnCanExecuteChanged();
 		}
 	}
 }
